7d667784f2dc6627fa1362789f9e4f08bbe16420
clandriot <cyril.landriot@gmail.com>
2018-02-04 23:12:51 +0100

    tricks to support heroku pipeline

--- scripts/replace.build.js
+++ scripts/replace.build.js
@@ -1,15 +1,9 @@
-var replace = require('replace-in-file');
+var replace = require("replace");
+var path = 'src/environments/environment.prod.ts';
 var apiUrl = process.env.API_URL;
-const prod = {
-  files: 'src/environments/environment.prod.ts',
-  from: /{API_URL}/g,
-  to: apiUrl,
-  allowEmptyPaths: false,
-};
-try {
-  let changedFiles = replace.sync(prod);
-  console.log('API Url set: ' + apiUrl);
-}
-catch (error) {
-  console.error('Error occurred:', error);
-}
+
+replace({
+  regex: '.*apiUrl.*',
+  replacement: '\tapiUrl: \'' + apiUrl + '\'',
+  paths: [path]
+});
